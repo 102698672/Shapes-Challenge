2be7a43baec561c09ab64f5f38e46ab8e570e4f3
102698672 <48079141+102698672@users.noreply.github.com>
2019-08-12 13:34:43 +1000

    shapes challenge upload

---
+++ shapes challenge/shapes challenge/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+namespace shapes_challenge
+{
+    public class Shape
+    {
+        public Color Colour;
+    }
+
+    public class Quadrilateral : Shape
+    {
+        public int GrossWeight;
+
+        public Quadrilateral(int side1length, int side2length, int side3length, int side4length)
+        {
+            int s1 = side1length;
+            int s2 = side2length;
+            int s3 = side3length;
+            int s4 = side4length;
+        }
+
+        public void GetPerimetre()
+        {
+
+        }
+    }
+}
+ 
